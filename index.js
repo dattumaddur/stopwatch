a032690c6186e63f7621917798650f16fabb57f8
CORP/dm185353 <dm185353@ncr.com>
2021-04-23 11:04:03 +0530

    Added friendship calculator

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ function startWatch(){
     document.getElementById("time").innerHTML=time.toLocaleTimeString();
 
 }
+
+function calculateLove(){
+    var p=Math.floor(Math.random()*100);
+    var str;
+   if(p>50){
+  str="Your percentage is "+p+" You are best friends!!"
+   }
+   else{
+        str="Your percentage is "+p+" You are just friends ;)"
+   }
+ $("#lol").text(str);
+ 
+ 
+}
